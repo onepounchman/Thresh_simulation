ee8e8959aac7410e1d68a0064839293170785c09
wenbo <563596410@qq.com>
2020-10-03 22:19:06 -0700

    Update test_simulation_v2.R

--- test_simulation_v2.R
+++ test_simulation_v2.R
@@ -9,20 +9,20 @@ source("simu_functions_v2.R")
 # Examples:
 ## 1. non-covariate (model:'logit')
 dat_logit<-simu(Risk=0.1,p0=1,p1=1,cov='non_covariate',model='logit') 
-data1<-sim_thre(dat_logit,n = 1000,rndseed=2,p=c(0.01,0.03,0.05,0.07,0.09))
-mean(data$Y)
+data1<-sim_thre(dat_logit,n = 1000,rndseed=1,p=c(0.01,0.03,0.05,0.07,0.09))
+mean(data1$simmed_data$Y)
 plotDAG(dat_logit)
 
 ## 2. non_confound (model: 'logit')
 dat_logit2<-simu(Risk=0.1,p0=1,p1=1,cov='non_confound',model='logit') 
-data2<-sim(dat_logit2,n = 1000,rndseed=1)
-mean(data2$Y)
+data2<-sim_thre(dat_logit2,n = 1000,rndseed=1,p=c(0.01,0.03,0.05,0.07,0.09))
+mean(data2$simmed_data$Y)
 plotDAG(dat_logit2)
 
 ## 3. confound (model: 'probit') confounder W3 
 dat_probit<-simu(Risk=0.1,p0=1,p1=1,cov='confound',model='probit') 
 data3<-sim_thre(dat_probit,n = 1000,rndseed=1,p=c(0.01,0.03,0.05,0.07,0.09))
-mean(data3$Y)
+mean(data3$simmed_data$Y)
 plotDAG(dat_probit)
 
 
