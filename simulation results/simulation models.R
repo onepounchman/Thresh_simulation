f604212e429d48168c35697694614e609c8a2936
wenbo <563596410@qq.com>
2020-10-25 14:15:50 -0700

    simulation results

---
+++ simulation results/simulation models.R
@@ -0,0 +1,22 @@
+# non-counfounding case
+D <- D +
+  node("W1", distr = "runif", min = -1, max = 1) +
+  node("W2", distr = "rnorm", mean = 0) +
+  node("W3full", distr = "rexp", rate = 1) +
+  node("W3", distr = "rconst", const = min(W3full,3))+
+  node("Sfull", distr = "rgamma", shape = abs(2.5)  ) +
+  node("S",distr = "rconst", const = min(Sfull, 13)/13)+
+  node("PY", distr = "rconst", const = logit(0.7*(0.5-7*S + W1+W2+2*W1*(W2 >0) - 2*W2*(W1>0) + W3 * (S-0.1) - W3*cos(W1)))) +
+  node("Y", distr = "rbinom", size =1, prob = PY)
+
+
+# confounding case
+D <- D +
+  node("W1", distr = "runif", min = -1, max = 1) +
+  node("W2", distr = "rnorm", mean = 0) +
+  node("W3full", distr = "rexp", rate = 1) +
+  node("W3", distr = "rconst", const = min(W3full,3))+
+  node("Sfull", distr = "rgamma", shape = abs(2 + W3 + W2*exp(W1) + cos(W2) + sin(W1))) +
+  node("S",distr = "rconst", const = min(Sfull, 13)/13)+
+  node("PY", distr = "rconst", const = logit(0.7*(1-7*S + W1+W2+2*W1*(W2 >0) - 2*W2*(W1>0) + W3 * (S-0.1) - W3*cos(W1)))) +
+  node("Y", distr = "rbinom", size =1, prob = PY)
